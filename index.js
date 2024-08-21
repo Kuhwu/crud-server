8be228a341653050e2fd0141a72434195db99f8b
Kawu <138832632+Kawudesu@users.noreply.github.com>
2024-08-21 20:41:29 +0800

    UPDATE
    - PostAPI
        -not yet finish

--- index.js
+++ index.js
@@ -2,8 +2,41 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+app.use(express.urlencoded({
+    extended:true
+}))
+
+const studentData = [];
 
 port = 3000;
 app.listen(port,() =>{
     console.log(`Successfully Connected to ${port}`)
 });
+
+
+//PostAPI
+
+app.post("/api/add_student",(req,res)=>{
+    const pdata = {
+        "id": studentData.length+1,
+        "firstName": req.body.fname,
+        "lastName": req.body.lname,
+        "course": req.body.course,
+        "year": req.body.year,
+        "enrolled": req.body.enrolled
+    }
+
+    studentData.push(sdata);
+    console.log("Final Result", sdata);
+
+
+    res.statusCode(200).send({
+        "Status Code": 200,
+        "Message": "Student has been added successfully",
+        "student":sdata
+});
+
+})
+
